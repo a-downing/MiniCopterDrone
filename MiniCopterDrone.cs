80d51e644b433e0953ce409eb753d29a7a207eb2
Andrew Downing <andrew2085@gmail.com>
2019-09-25 14:42:05 -0400

    version

--- MiniCopterDrone.cs
+++ MiniCopterDrone.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Minicopter Drone", "Andrew", "1.1.0")]
+    [Info("Minicopter Drone", "Andrew", "1.2.1")]
     public class MiniCopterDrone : RustPlugin {
         static MiniCopterDrone plugin = null;
         DroneManager droneManager = null;
